d1ec19147d4e6526bfd573e4fa72cd065bd3b7ad
Steve Calla <callasteven@gmail.com>
2022-08-17 06:09:16 -0600

    Add draft phone validation code

--- assets/js-update-portfolio/validatePhone.js
+++ assets/js-update-portfolio/validatePhone.js
@@ -9,3 +9,20 @@
 // validate('123-345-3456')   // true
 
 {/* <a href="tel:+1(201)934-7068">${phoneNumber}</a> */}
+
+// https://github.com/SBoudrias/Inquirer.js/blob/master/packages/inquirer/examples/pizza.js
+// {
+//   type: 'input',
+//   name: 'phone',
+//   message: "What's your phone number?",
+//   validate(value) {
+//     const pass = value.match(
+//       /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+//     );
+//     if (pass) {
+//       return true;
+//     }
+
+//     return 'Please enter a valid phone number';
+//   },
+// },
